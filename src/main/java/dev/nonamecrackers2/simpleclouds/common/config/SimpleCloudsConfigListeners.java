84d43cd919141604a92d97cacba54030bcdbdae4
nonamecrackers2 <nonamecrackers2@outlook.com>
2024-09-03 15:06:39 -0600

    Import organization

--- src/main/java/dev/nonamecrackers2/simpleclouds/common/config/SimpleCloudsConfigListeners.java
+++ src/main/java/dev/nonamecrackers2/simpleclouds/common/config/SimpleCloudsConfigListeners.java
@@ -1,15 +1,11 @@
 package dev.nonamecrackers2.simpleclouds.common.config;
 
 import dev.nonamecrackers2.simpleclouds.SimpleCloudsMod;
-import dev.nonamecrackers2.simpleclouds.client.config.SimpleCloudsClientConfigListeners;
 import dev.nonamecrackers2.simpleclouds.common.cloud.CloudMode;
 import dev.nonamecrackers2.simpleclouds.common.packet.SimpleCloudsPacketHandlers;
 import dev.nonamecrackers2.simpleclouds.common.packet.impl.update.NotifyCloudModeUpdatedPacket;
 import dev.nonamecrackers2.simpleclouds.common.packet.impl.update.NotifySingleModeCloudTypeUpdatedPacket;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.util.thread.EffectiveSide;
 import net.minecraftforge.network.PacketDistributor;
 import nonamecrackers2.crackerslib.common.config.listener.ConfigListener;
 
